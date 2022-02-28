72ffe6bfe9a25f8ef03de36a77e3ad2fd7de3d04
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-28 16:02:39 +0100

    Class "Product" created + getter and setter of property created

--- product.php
+++ product.php
@@ -5,3 +5,92 @@ I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
 L’utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
 Il pagamento avviene con la carta di credito, che non deve essere scaduta.
 */
+
+
+class Product
+{
+    protected bool $available;
+    protected int $price;
+    protected string $brand;
+    protected string $name;
+
+    /**
+     * Get the value of available
+     */ 
+    public function getAvailable()
+    {
+        return $this->available;
+    }
+
+    /**
+     * Set the value of available
+     *
+     * @return  self
+     */ 
+    public function setAvailable($available)
+    {
+        $this->available = $available;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */ 
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     *
+     * @return  self
+     */ 
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of brand
+     */ 
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Set the value of brand
+     *
+     * @return  self
+     */ 
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
