05ec3d3547727bdfaefe9f897d5a21d7b2e05ed2
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-28 18:49:36 +0100

    User Class Created

---
+++ user.php
@@ -0,0 +1,78 @@
+<?php
+
+class User
+{
+    protected string $name;
+    protected string $surname;
+    protected bool $isRegistered;
+    protected string $id;
+
+    function __construct($_name, $_surname, $_isRegistered, $_id)
+    {
+        $this->setName($_name);
+        $this->setName($_surname);
+        $this->setName($_isRegistered);
+        $this->setName($_id);
+    }
+
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of surname
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set the value of surname
+     *
+     * @return  self
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
